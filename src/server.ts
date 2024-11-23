253e4cf9aeef04e21b1473b16210e95c63f35903
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-11-23 14:54:48 +0900

    feat:prepare for production

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 /** @format */
 
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+   path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+});
 import mongoose from "mongoose";
 import server from "./app";
 mongoose
